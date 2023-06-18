84cd50736be6eb41a9e1d438e7f34aedd2e5e5bc
SOFe <sofe2038@gmail.com>
2023-06-18 11:20:42 +0800

    fix: invoke control loop when starting to display

--- src/plugin.php
+++ src/plugin.php
@@ -11,6 +11,7 @@ use pocketmine\player\Player;
 use pocketmine\plugin\Plugin;
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\TextFormat;
+use SOFe\AwaitGenerator\Await;
 use SOFe\Zleep\Zleep;
 use WeakMap;
 use function count;
@@ -50,10 +51,10 @@ final class MainClass extends PluginBase {
 					$session->reset();
 					break;
 				case "show":
-					$session->display = true;
+					$session->show();
 					break;
 				case "hide":
-					$session->display = false;
+					$session->hide();
 					break;
 				default:
 					$sender->sendMessage(TextFormat::RED . "Unknown subcommand \"$subcmd\"");
@@ -66,9 +67,10 @@ final class MainClass extends PluginBase {
 }
 
 final class Session {
-	public bool $display = false;
-	public ?float $relativeStart = null;
-	public ?float $pausedDuration = null;
+	private bool $display = false;
+	private ?float $relativeStart = null;
+	private ?float $pausedDuration = null;
+	private bool $looping = false;
 
 	public function __construct(
 		private Plugin $plugin,
@@ -76,20 +78,25 @@ final class Session {
 	) {
 	}
 
-	public function loop() : Generator {
-		while ($this->player->isOnline()) {
-			if ($this->display) {
-				$duration = 0.0;
-				if ($this->relativeStart !== null) {
-					$duration = microtime(true) - $this->relativeStart;
-				} elseif ($this->pausedDuration !== null) {
-					$duration = $this->pausedDuration;
+	private function loop() : Generator {
+		$this->looping = true;
+		try {
+			while ($this->player->isOnline() && $this->display) {
+				if ($this->display) {
+					$duration = 0.0;
+					if ($this->relativeStart !== null) {
+						$duration = microtime(true) - $this->relativeStart;
+					} elseif ($this->pausedDuration !== null) {
+						$duration = $this->pausedDuration;
+					}
+
+					$this->player->sendPopup(self::formatDuration($duration));
 				}
 
-				$this->player->sendPopup(self::formatDuration($duration));
+				yield from Zleep::sleepSeconds($this->plugin, 1.);
 			}
-
-			yield from Zleep::sleepSeconds($this->plugin, 1.);
+		} finally {
+			$this->looping = false;
 		}
 	}
 
@@ -112,6 +119,17 @@ final class Session {
 		} else {
 			$this->pausedDuration = 0.0;
 		}
+	}
+
+	public function show() : void {
+		$this->display = true;
+		if(!$this->looping) {
+			Await::g2c($this->loop());
+		}
+	}
+
+	public function hide() : void {
+		$this->display = false;
 	}
 
 	private function getDuration() : float {
